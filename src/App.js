4dbd3b2243dc4bd5efc2bc47c192b55b014b5672
whymidnight <45319061+whymidnight@users.noreply.github.com>
2022-02-10 06:40:41 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./styles.css";
+import { Canvas } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Suspense } from "react";
+
+const Model = () => {
+  const gltf = useLoader(GLTFLoader, "./Poimandres.gltf");
+  return (
+    <>
+      <primitive object={gltf.scene} scale={0.4} />
+    </>
+  );
+};
+
+export default function App() {
+  return (
+    <div className="App">
+      <Canvas>
+        <Suspense fallback={null}>
+          <Model />
+          <OrbitControls />
+          <Environment preset="sunset" background />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+}
