eb0b5472deccefaaaad36284583767554ca70b39
Dominic DiGiacomo <ddigiacomo@triptychlabs.io>
2022-02-26 09:13:11 -0800

    init commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ import { Environment, OrbitControls } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { Suspense } from "react";
 
-const Model = () => {
-  const gltf = useLoader(GLTFLoader, "./Poimandres.gltf");
+const Ship = () => {
+  const gltf = useLoader(GLTFLoader, "./ship.glb");
+  return (
+    <>
+      <primitive object={gltf.scene} scale={0.4} />
+    </>
+  );
+};
+
+const Land = () => {
+  const gltf = useLoader(GLTFLoader, "./land.glb");
+  gltf.scene.position.setY(-5);
   return (
     <>
       <primitive object={gltf.scene} scale={0.4} />
@@ -19,9 +29,10 @@ export default function App() {
     <div className="App">
       <Canvas>
         <Suspense fallback={null}>
-          <Model />
+          <Land />
+          <Ship />
           <OrbitControls />
-          <Environment preset="sunset" background />
+          <Environment preset="sunset"/>
         </Suspense>
       </Canvas>
     </div>
